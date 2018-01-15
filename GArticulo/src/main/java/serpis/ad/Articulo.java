ca5c383b509f122eaed61b1f9a2d0e0460681c5c
edudejuan <e_d_u4@hotmail.com>
2018-01-15 12:31:22 +0100

    Actualizados GHibernate y GArticulo, creado GVenta

--- GArticulo/src/main/java/serpis/ad/Articulo.java
+++ GArticulo/src/main/java/serpis/ad/Articulo.java
@@ -1,12 +1,40 @@
 package serpis.ad;
 
+import java.math.BigDecimal;
+
 public class Articulo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private long id;
+	private long categoria;
+	private String nombre;
+	private BigDecimal precio;
+	
+	public Articulo(long id, long categoria, String nombre, BigDecimal precio) {
+		
+	
 
 	
 	
+	}
+
+	public long getCategoria() {
+		// TODO Auto-generated method stub
+		return categoria;
+	}
+
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return nombre;
+	}
+
+	public BigDecimal getPrecio() {
+		// TODO Auto-generated method stub
+		return precio;
+	}
+
+	public long getId() {
+		// TODO Auto-generated method stub
+		return id;
 	}
 
 }
--- GArticulo/src/main/java/serpis/ad/ArticuloDao.java
+++ GArticulo/src/main/java/serpis/ad/ArticuloDao.java
@@ -7,15 +7,40 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ArticuloDao {
-
-	public static void load(Connection connection) throws SQLException{
+	
+	public static void load(Connection connection) throws SQLException {
 		try (Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(App.sqlQuery); ) {
 			while (rs.next()) {
-				int numColumns = rs.getMetaData.get)
+				int numColumns = rs.getMetaData().getColumnCount();
+				for (int i = 1; i <= numColumns; i++) {
+					//TODO
+				}
 			}
 		}
-						
 	}
+
+
+
+public static void insert(Connection connection, Articulo articulo) throws SQLException {
+	//TODO
+}
+
+public static void update(Connection connection, Articulo articulo) throws SQLException {
+	PreparedStatement stmtArticulo;
+	String updateArticulo;
+	
+	updateArticulo = "UPDATE articulo SET categoria = ?, nombre = ?, precio = ? WHERE id = ?";
+	
+	stmtArticulo = connection.prepareStatement(updateArticulo);
+	
+	stmtArticulo.setLong(1, articulo.getCategoria());
+	stmtArticulo.setString(2, articulo.getNombre());
+	stmtArticulo.setBigDecimal(3, articulo.getPrecio());
+	stmtArticulo.setLong(4, articulo.getId());
+	
+	stmtArticulo.executeUpdate();
+	
+}
 
 }
